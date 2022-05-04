59f03d95ea7818f1f4065dda93b72e50945ccffd
Nick Morrish <32886269+nmorrish@users.noreply.github.com>
2022-05-04 00:03:17 -0600

    Initial Create
    Project as completed on May 03, 2022

---
+++ StudentHelp/Entities/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentHelp.Entities
+{
+    public class Course
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
